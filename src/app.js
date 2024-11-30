63886e581d1e59f95efb06c136088048a3f82cbf
Updesh Kumar <updesh_kumar@srmap.edu.in>
2024-11-30 11:54:02 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,4 +10,12 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static("public"))
 //app.use(cookieParser())
+
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
 export default app
